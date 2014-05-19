de3f759f6dd885bd38a77bed23cc1b43d5543acc
Aymerick <aymerick@jehanne.org>
2014-05-19 18:00:54 +0200

    Added test

---
+++ ember-withcli/tests/integration/landing-page-test.js
@@ -0,0 +1,18 @@
+import startApp from 'ember-withcli/tests/helpers/start-app';
+
+var App;
+
+module('Integration - Landing Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should welcome me to Boston Ember', function() {
+  visit('/').then(function() {
+    equal(find('h2#title').text(), 'Welcome to Ember.js');
+  });
+});
